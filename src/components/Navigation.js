0f7a89fd7a50862bc94194cf04d26eb9f31dd2c2
JaredS129 <jaredkevin.stevenson@gmail.com>
2024-07-10 23:49:13 +0200

    remove tooltip on expanded nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,31 +12,36 @@ import useWindowDimensions from "../hooks/useWindowDimensions";
 
 function Navigation() {
   const { width } = useWindowDimensions();
+  const [collapsed, setCollapsed] = useState(true);
+  const [currentPage, setCurrentPage] = useState("trangchủ");
+
   const items = [
     {
       label: "Trang Chủ",
       key: "trangchủ",
+      title: width < 768 && collapsed ? "Trang Chủ" : null,
       icon: <HomeOutlined />,
     },
     {
       label: "Tìm Hiểu Thêm",
       key: "tìmhiểuthêm",
+      title: width < 768 && collapsed ? "Tìm Hiểu Thêm" : null,
       icon: <InfoCircleOutlined />,
     },
     {
       label: "Liên Hệ",
       key: "hiênhệ",
+      title: width < 768 && collapsed ? "Liên Hệ" : null,
       icon: <ContactsOutlined />,
     },
     {
       label: "Địa chỉ",
       key: "dịachỉ",
+      title: width < 768 && collapsed ? "Địa chỉ" : null,
       icon: <NodeIndexOutlined />,
     },
   ];
 
-  const [currentPage, setCurrentPage] = useState("trangchủ");
-  const [collapsed, setCollapsed] = useState(true);
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
